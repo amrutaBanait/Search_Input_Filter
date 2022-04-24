a1477947ae1b827d24ac2ca7e639e776e11a11fa
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-24 20:52:45 +0530

    commited

--- Data/src/App.jsx
+++ Data/src/App.jsx
@@ -18,20 +18,50 @@ function App() {
     fetchPeople();
   }, []);
 
+  const searchPeople = (searchValue) => {
+    setSearchInput(searchValue);
+
+    if (searchInput) {
+      const filterdSearch = people.filter((people) =>(
+        Object.values(person)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      ))
+      setFiltered(filterdSearch)
+    }else{
+      setFiltered(people)
+    }
+  }
+
   return (
     <>
- <div className="my-10">
- <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
-        {people.map(({ id, name, image, dob }) => (
-          <article key={id} className="bg-yellow-300 p-5 rounded shadow">
-            <img src={image} alt={name} title={name} className="h-96 w-full" />
-            <h3 className="font-bold text-xl mt-1">{name}</h3>
-            <p>DOB: {dob}</p>
-          </article>
-  ))}
-      </section>
- </div>
-    </> 
+      <div className="my-10">
+        <input
+          type="text"
+          name="text"
+          id="text"
+          placeholder="Search..."
+          onChange={(e) => searchPeople(e.target.value)}
+          autoComplete="off"
+        />
+
+        <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
+          {people.map(({ id, name, image, dob }) => (
+            <article key={id} className="bg-yellow-300 p-5 rounded shadow">
+              <img
+                src={image}
+                alt={name}
+                title={name}
+                className="h-96 w-full"
+              />
+              <h3 className="font-bold text-xl mt-1">{name}</h3>
+              <p>DOB: {dob}</p>
+            </article>
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
