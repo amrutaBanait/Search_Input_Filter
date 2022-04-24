19fd15cb80938952511a6221b3b749ea26f48aff
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-24 20:43:57 +0530

    commited

--- Data/src/App.jsx
+++ Data/src/App.jsx
@@ -20,13 +20,13 @@ function App() {
 
   return (
     <>
- <div>
+ <div className="my-10">
  <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
         {people.map(({ id, name, image, dob }) => (
-          <article key={id}>
+          <article key={id} className="bg-yellow-300 p-5 rounded shadow">
             <img src={image} alt={name} title={name} className="h-96 w-full" />
-            <h3>{name}</h3>
-            <p>{dob}</p>
+            <h3 className="font-bold text-xl mt-1">{name}</h3>
+            <p>DOB: {dob}</p>
           </article>
   ))}
       </section>
