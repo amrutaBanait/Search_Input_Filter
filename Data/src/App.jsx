d27c68f48ac780ef05e99e8eac0281b02a980a31
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-24 23:46:48 +0530

    commited

--- Data/src/App.jsx
+++ Data/src/App.jsx
@@ -22,60 +22,73 @@ function App() {
     setSearchInput(searchValue);
 
     if (searchInput) {
-      const filterdSearch = people.filter((people) =>(
+      const filterdSearch = people.filter((people) =>
         Object.values(person)
           .join("")
           .toLowerCase()
           .includes(searchValue.toLowerCase())
-      ))
-      setFiltered(filterdSearch)
-    }else{
-      setFiltered(people)
+      );
+      setFiltered(filterdSearch);
+    } else {
+      setFiltered(people);
     }
-  }
+  };
 
   return (
     <>
-      <div className="my-10">
-        <h1 className="text-white uppercase font-bold text-2xl md:text-4xl lg:text-6xl text-center mb-10">Search Input Filter</h1>
-        <input
-          type="text"
-          name="text"
-          id="text"
-          placeholder="Search..."
-          onChange={(e) => searchPeople(e.target.value)}
-          autoComplete="off"
-          className="w-1/2 block mx-auto py-2 px-5 rounded shadow mb-10"
-        />
+      {isLoading ? (
+        <h1 className="text-white uppercase font-bold
+         text-2xl md:text-4xl lg:text-6xl text-center mb-10 flex items-center justify-center h-screen">
+          Loading...
+        </h1>
+      ) : (
+        <div className="my-10">
+          <h1 className="text-white uppercase font-bold text-2xl md:text-4xl lg:text-6xl text-center mb-10">
+            Search Input Filter
+          </h1>
+          <input
+            type="text"
+            name="text"
+            id="text"
+            placeholder="Search..."
+            onChange={(e) => searchPeople(e.target.value)}
+            autoComplete="off"
+            className="w-1/2 block mx-auto py-2 px-5 rounded shadow mb-10"
+          />
 
-    {searchInput.length > 1 ?     <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
-          {filtered.map(({ id, name, image, dob }) => (
-            <article key={id} className="bg-yellow-300 p-5 rounded shadow">
-              <img
-                src={image}
-                alt={name}
-                title={name}
-                className="h-96 w-full"
-              />
-              <h3 className="font-bold text-xl mt-1">{name}</h3>
-              <p>DOB: {dob}</p>
-            </article>
-          ))}
-        </section> :     <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
-          {people.map(({ id, name, image, dob }) => (
-            <article key={id} className="bg-yellow-300 p-5 rounded shadow">
-              <img
-                src={image}
-                alt={name}
-                title={name}
-                className="h-96 w-full"
-              />
-              <h3 className="font-bold text-xl mt-1">{name}</h3>
-              <p>DOB: {dob}</p>
-            </article>
-          ))}
-        </section>}
-      </div>
+          {searchInput.length > 1 ? (
+            <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
+              {filtered.map(({ id, name, image, dob }) => (
+                <article key={id} className="bg-yellow-300 p-5 rounded shadow">
+                  <img
+                    src={image}
+                    alt={name}
+                    title={name}
+                    className="h-96 w-full"
+                  />
+                  <h3 className="font-bold text-xl mt-1">{name}</h3>
+                  <p>DOB: {dob}</p>
+                </article>
+              ))}
+            </section>
+          ) : (
+            <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
+              {people.map(({ id, name, image, dob }) => (
+                <article key={id} className="bg-yellow-300 p-5 rounded shadow">
+                  <img
+                    src={image}
+                    alt={name}
+                    title={name}
+                    className="h-96 w-full"
+                  />
+                  <h3 className="font-bold text-xl mt-1">{name}</h3>
+                  <p>DOB: {dob}</p>
+                </article>
+              ))}
+            </section>
+          )}
+        </div>
+      )}
     </>
   );
 }
