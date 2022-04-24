b03410cb14c0e7b9c89c9777b2063db9e9cf47d6
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-24 20:36:18 +0530

    commited

--- Data/src/App.jsx
+++ Data/src/App.jsx
@@ -1,32 +1,36 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
-const url = `https://black-history-month-api.herokuapp.com/people`
+const url = `https://black-history-month-api.herokuapp.com/people`;
 
-function App(){
-  const [people, setPeople] = useState([])
-  const [filtered, setFiltered] = useState([])
-  const [searchInput, setSearchInput] = useState("")
+function App() {
+  const [people, setPeople] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
-const fetchPeople = async() => {
-  const res = await fetch(url)
-  const data = await res.json()
-  setPeople(data)
-}
+  const fetchPeople = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setPeople(data);
+  };
 
-useEffect (() => {
-  fetchPeople()
-}, [])
+  useEffect(() => {
+    fetchPeople();
+  }, []);
 
-  return(
+  return (
     <>
-<section>
-  <h1>{people.length} people</h1>
-</section>
-
+      <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
+        {people.map(({ id, name, image, dob }) => (
+          <article key={id}>
+            <img src={image} alt={name} title={name} />
+            <h3>{name}</h3>
+            <p>{dob}</p>
+          </article>
+  ))}
+      </section>
     </>
-  )
+  );
 }
-
 
 export default App;
