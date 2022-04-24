00bdc250bcfdae962e18a4aeae60b3330da89f73
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-24 20:41:28 +0530

    commited

--- Data/src/App.jsx
+++ Data/src/App.jsx
@@ -20,16 +20,18 @@ function App() {
 
   return (
     <>
-      <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
+ <div>
+ <section className="px-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:max-w-5xl xl:mx-auto">
         {people.map(({ id, name, image, dob }) => (
           <article key={id}>
-            <img src={image} alt={name} title={name} />
+            <img src={image} alt={name} title={name} className="h-96 w-full" />
             <h3>{name}</h3>
             <p>{dob}</p>
           </article>
   ))}
       </section>
-    </>
+ </div>
+    </> 
   );
 }
 
